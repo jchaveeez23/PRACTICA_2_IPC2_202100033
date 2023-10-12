705c10195fdf96df00c3af02fc5d2336e0c829a9
Josue Chavez <3921213790101@ingenieria.usac.edu.gt>
2023-10-11 22:51:08 -0600

    solo falta foto

--- Practica-2/Ejemplo/Curriculum/views.py
+++ Practica-2/Ejemplo/Curriculum/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 def PrimeraPagina(request):
-    return render(request, 'PaginaEjemplo/pagina.html', {})
+    return render(request, 'PaginaEjemplo/cv.html', {})
 # Create your views here.
--- Practica-2/Ejemplo/Ejemplo/urls.py
+++ Practica-2/Ejemplo/Ejemplo/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cv/', include('Curriculum.urls'))
+    path('', include('Curriculum.urls'))
 ]
